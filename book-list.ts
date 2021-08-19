fc61f193fb2dd1123682ed99454285a62635ea4a
totuslink <summerbud@totuslink.com>
2021-08-19 16:38:46 +0800

    🎉 feat(data): add dedicated data file

---
+++ book-list.ts
@@ -0,0 +1,12 @@
+const bookList = {
+	reading: [
+		{
+			title: 'Get together',
+			isbn: '978-1732265196',
+			readingChapter: '3',
+			startDate: '2021-08-12',
+		},
+	],
+};
+
+export default bookList;
