3cb80188f71c4e42ad6ce45c4c5bb9901eb51f74
totuslink <summerbud@totuslink.com>
2021-08-19 17:34:31 +0800

    🎉 feat(type): add dedicated type file

---
+++ type/type.ts
@@ -0,0 +1,11 @@
+export interface Book {
+	title: string;
+	subtitle: string;
+	authors: string;
+	publishedDate: string;
+	publisher: string;
+	description: string;
+	language: string;
+	pageCount: number;
+	image: string;
+}
