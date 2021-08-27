f11decf0c163502fad821bcd1deb5802f209b64d
totuslink <summerbud@totuslink.com>
2021-08-27 15:12:47 +0800

    🎉 feat(recommend): add style

--- components/BooksList.tsx
+++ components/BooksList.tsx
@@ -7,22 +7,21 @@ interface Props {
 }
 
 const BooksList: FC<Props> = ({ books }) => {
+	console.log(books);
 	return (
-		<div className="font-sans grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+		<div className="font-sans grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
 			{books.map((book, index) => (
 				<div key={index}>
 					<div className="flex flex-col">
-						{ book.image ? (<div className="relative mx-auto mb-4">
-							<Image src={book.image} alt={book.title} width={192} height={288} />
-						</div>) : (<div className="w-[128px] h-[192px]"></div>)}
-            <div
-							className="w-[192px] mx-auto font-sans font-semibold text-sdm-cg-100"
-						>
-              {book.title}
-            </div>
-            <div className="w-[192px] mx-auto font-sans font-normal text-sdm-steel-blue-300">
-              {book.authors[0]}
-            </div>
+						{book.image ? (
+							<div className="flex mx-auto mb-4">
+								<Image src={book.image} alt={book.title} width={192} height={288} />
+							</div>
+						) : (
+							<div className="w-[192px] h-[288px] mb-4"></div>
+						)}
+						<div className="w-[192px] mx-auto font-sans font-semibold text-sdm-cg-100">{book.title}</div>
+						{book.authors && <div className="w-[192px] mx-auto font-sans font-normal text-sdm-steel-blue-300">{book.authors[0]}</div>}
 					</div>
 				</div>
 			))}
